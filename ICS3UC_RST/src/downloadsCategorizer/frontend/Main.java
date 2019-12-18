630f49b87e8d55b31fb4e95d2a5904447067321d
finlaywashere <finman292004@protonmail.com>
2019-12-18 09:52:51 -0500

    Got the UI working

--- ICS3UC_RST/src/downloadsCategorizer/frontend/Main.java
+++ ICS3UC_RST/src/downloadsCategorizer/frontend/Main.java
@@ -1,20 +1,33 @@
 package downloadsCategorizer.frontend;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import downloadsCategorizer.common.ConfigurationManager;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	private GridPane fileListPane;
+	private Map<String,List<File>> indexes;
+	private ObservableList<Label> labels = FXCollections.observableArrayList();
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		this.indexes = ConfigurationManager.loadIndexes();
 		GridPane root = new GridPane();
 		root.setAlignment(Pos.TOP_CENTER);
 		root.setHgap(10);
@@ -30,23 +43,28 @@ public class Main extends Application {
 			search(field.getText());
 		});
 		root.add(button, 2, 0);
-		
-		fileListPane = new GridPane();
-		fileListPane.setAlignment(Pos.TOP_LEFT);
-		fileListPane.setHgap(10);
-		fileListPane.setVgap(10);
-		fileListPane.setPadding(new Insets(20,20,20,20));
-		
-		Label test = new Label("test");
-		fileListPane.add(test, 0, 1);
-		
-		root.add(fileListPane, 1, 3);
+		ListView<Label> fileListView = new ListView<Label>(labels);
+		root.add(fileListView, 1, 3);
 		primaryStage.setScene(new Scene(root,1920,1080));
 		primaryStage.show();
 		button.requestFocus();
 	}
 	private void search(String filename) {
-		
+		List<File> files = indexes.get(filename);
+		labels.clear();
+		if(files == null) return;
+		for(File f : files) {
+			Label l = new Label(f.getPath());
+			l.setOnMouseClicked(event -> {
+				Desktop desktop = Desktop.getDesktop();
+				try {
+					desktop.open(f.getParentFile());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			labels.add(l);
+		}
 	}
 
 	public static void main(String[] args) {
