8ac824968b5c7ab4d90258bba1e16846f92362c7
finlaywashere <finman292004@protonmail.com>
2019-12-19 09:46:13 -0500

    🦀🦀 Tests are done 🦀🦀

--- ICS3UC_RST/src/test/downloadsCategorizer/daemon/DaemonUtilsTest.java
+++ ICS3UC_RST/src/test/downloadsCategorizer/daemon/DaemonUtilsTest.java
@@ -1,14 +1,22 @@
 package downloadsCategorizer.daemon;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class DaemonUtilsTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCategorize() {
+		List<String> patterns = new ArrayList<String>();
+		patterns.add("*.txt Text");
+		patterns.add("*.jpg Images");
+		File folder = DaemonUtils.categorizeFile("test.jpg", patterns);
+		if(!folder.getName().equals("Images"))
+			fail();
 	}
 
 }
